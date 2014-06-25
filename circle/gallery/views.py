cdd478caa8adcde778a83df5baa49a5a7724a6cb
azure provisioned user <azureuser@absurd.absurd.b2.internal.chinacloudapp.cn>
2014-06-25 11:09:48 +0000

    Oops

--- circle/gallery/views.py
+++ circle/gallery/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+# _*_ coding:utf-8 _*_
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    return render_to_response('index.html')
